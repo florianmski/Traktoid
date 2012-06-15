a7380b3fb92836432d3aa42c65bd537bb64b9c50
Florian <florian@BikiniBottom.(none)>
2012-06-16 01:25:25 +0200

    try submodule

--- trakt-java/src/main/java/com/jakewharton/apibuilder/package-info.java
+++
@@ -1,5 +0,0 @@
-/**
- * Classes which assist in marshalling the native builder methods to proper
- * remote API calls and the resulting JSON to POJOs for RESTful APIs.
- */
-package com.jakewharton.apibuilder;
